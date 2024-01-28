d915abc2550de4be9c313b721281cea1a6c75447
Nikolaj Kim <nikolajkkim@gmail.com>
2024-01-28 04:23:26 -0800

    update for user post, making it look white and less gray

--- munch-app/src/homeScreen.js
+++ munch-app/src/homeScreen.js
@@ -1,4 +1,5 @@
 import { FlatList, View, TouchableOpacity, StyleSheet, SafeAreaView, Image, Text } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 import * as React from 'react';
 
 const HomeScreen = ({ navigation }) => {
@@ -12,12 +13,16 @@ const HomeScreen = ({ navigation }) => {
     // Add your right button logic here
   };
 
+  const handlePhotoTakingButtonPress = () => {
+    navigation.navigate('Post Screen')
+  }
+
   const feedData = [
     { id: '1', image: require('./images/stevenlee.jpg'), caption: 'Beautiful sunset' },
-    { id: '2', image: require('./images/stevenlee.jpg'), caption: 'Delicious meal' },
-    { id: '3', image: require('./images/stevenlee.jpg'), caption: 'I love COCO!' },
-    { id: '4', image: require('./images/stevenlee.jpg'), caption: 'TASTEA is AWESOME' },
-    { id: '5', image: require('./images/stevenlee.jpg'), caption: 'Who wants to grab a bite?' },
+    { id: '2', image: require('./images/stevenbutt.png'), caption: 'Delicious meal' },
+    { id: '3', image: require('./images/stevenmee.png'), caption: 'I love COCO!' },
+    { id: '4', image: require('./images/stevenkim.png'), caption: 'TASTEA is AWESOME' },
+    { id: '5', image: require('./images/stevenyang.png'), caption: 'Who wants to grab a bite?' },
   ];
 
   const renderFeedItem = ({ item }) => (
@@ -26,6 +31,8 @@ const HomeScreen = ({ navigation }) => {
       <Text style={styles.caption}>{item.caption}</Text>
     </View>
   );
+  const route = useRoute();
+  //const { image } = route.params;
 
     return (
       <SafeAreaView style={styles.container}>
@@ -54,6 +61,7 @@ const HomeScreen = ({ navigation }) => {
               />
             </TouchableOpacity>
           </View>
+          
         </View>
 
         <FlatList
@@ -62,6 +70,15 @@ const HomeScreen = ({ navigation }) => {
           renderItem={renderFeedItem}
           style={styles.feedContainer}
         />
+        <View style={styles.cameraButton}>
+            {/* Camera Button */}
+            <TouchableOpacity onPress={handlePhotoTakingButtonPress} style={styles.button}>
+              <Image
+                source={require('./images/camera_button.png')}
+                style={styles.iconImage}
+              />
+            </TouchableOpacity>
+          </View>
       </SafeAreaView>
     );
 }
@@ -124,6 +141,11 @@ const styles = StyleSheet.create({
   caption: {
     marginTop: 8,
     fontSize: 16,
+  },
+  cameraButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 150,
   },
 });
 
